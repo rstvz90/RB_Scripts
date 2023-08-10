4f072db9e77f5735c31829b783e7756a5a1d08b3
rstvz90 <rodriestevez@hotmail.com>
2023-08-10 17:18:52 -0300

    ver acelerador en altos y bajos consumo

--- Análisis/Drive UP/Datos Conducción Eficiente.r
+++ Análisis/Drive UP/Datos Conducción Eficiente.r
@@ -18,6 +18,8 @@ for (paquete in listofpackages) {
   suppressMessages(library(paquete, character.only = TRUE))
 }
 
+# LECTURA DE ARCHIVOS ------------------------------
+
 path <- "C:/Users/REstevez/Documents/Conducción DriveUP/Datos Línea 41/"
 
 files <- list.files(path, full.names = TRUE)
@@ -48,44 +50,153 @@ chasis_41 <- fread(files_chasis)
 consumo_41 <- fread(files_consumo)
 ralenti_41 <- fread(files_ralenti)
 
-correlaciones <- merge.data.table(acelerador_41, consumo_41,
+# ACELERADOR -----------------------------------
+
+acelerador_cor <- merge.data.table(acelerador_41, consumo_41,
   by = "cod_desig",
   all.x = TRUE
 )
-correlaciones <- merge.data.table(correlaciones, chasis_41,
+acelerador_cor <- merge.data.table(acelerador_cor, chasis_41,
   by = "cod_desig",
   all.x = TRUE
 )
-correlaciones <- correlaciones[`Suma de Distance` > 50, ]
-correlaciones <- correlaciones[`Suma de consumption.avgFuel` > 20, ]
-colnames(correlaciones)
-ggplot(correlaciones) +
-  aes(x = `Suma de accelerator.get80`, y = `Suma de consumption.avgFuel`, color = Chasis) +
-  geom_point() +
-  geom_smooth(method = lm)
+acelerador_cor <- acelerador_cor[`Suma de Distance` > 50, ]
+acelerador_cor <- acelerador_cor[`Suma de consumption.avgFuel` > 20, ]
 
-correlaciones <- correlaciones[, c(2, 3, 4, 5, 6, 10, 13)]
+# ggplot(acelerador_cor) +
+#  aes(x = `Suma de accelerator.get80`, y = `Suma de consumption.avgFuel`, color = Chasis) +
+#  geom_point() +
+#  geom_smooth(method = lm)
 
-setnames(correlaciones, colnames(correlaciones), gsub("Suma de ", "", colnames(correlaciones)))
+acelerador_cor <- acelerador_cor[, c(2, 3, 4, 5, 6, 10, 13)]
 
-columnas <- colnames(correlaciones)
+setnames(acelerador_cor, colnames(acelerador_cor), gsub("Suma de ", "", colnames(acelerador_cor)))
 
-tabla <- list()
+columnas <- colnames(acelerador_cor)
+
+tabla_acelerador <- list()
 for (i in c(1:5)) {
-  tabla[[i]] <- correlaciones[, .(correlacion = cor(
-      get(columnas[i]),
-      get(columnas[6])
-    ), Pos = columnas[i]),
-    by = .(Chasis)
-    ]
+  tabla_acelerador[[i]] <- acelerador_cor[, .(correlacion = cor(
+    get(columnas[i]),
+    get(columnas[6])
+  ), Pos = columnas[i]),
+  by = .(Chasis)
+  ]
 }
-correlaciones <- rbindlist(tabla)
-correlaciones_matrix <- dcast(correlaciones, ...~Chasis, value.var = "correlacion")
+acelerador_cor <- rbindlist(tabla_acelerador)
+acelerador_cor_matrix <- dcast(acelerador_cor, ... ~ Chasis, value.var = "correlacion")
 
-ggplot(data = correlaciones) +
-  aes(x = Pos, y = correlacion, label = round(correlacion,2)) +
+ggplot(data = acelerador_cor) +
+  aes(x = Pos, y = correlacion, label = round(correlacion, 2)) +
   geom_bar(stat = "identity", position = "dodge", fill = "lightblue") +
   geom_text(nudge_y = 0.03) +
   facet_grid(~Chasis) +
   theme_minimal()
 
+
+# RPM -----------------------------------
+
+RPM_cor <- merge.data.table(rpm_41, consumo_41,
+  by = "cod_desig",
+  all.x = TRUE
+)
+RPM_cor <- merge.data.table(RPM_cor, chasis_41,
+  by = "cod_desig",
+  all.x = TRUE
+)
+RPM_cor <- RPM_cor[`Suma de Distance` > 50, ]
+RPM_cor <- RPM_cor[`Suma de consumption.avgFuel` > 20, ]
+
+# ggplot(acelerador_cor) +
+#  aes(x = `Suma de accelerator.get80`, y = `Suma de consumption.avgFuel`, color = Chasis) +
+#  geom_point() +
+#  geom_smooth(method = lm)
+colnames(RPM_cor)
+RPM_cor <- RPM_cor[, c(2:11, 13, 16, 19)]
+
+setnames(RPM_cor, colnames(RPM_cor), gsub("Suma de ", "", colnames(RPM_cor)))
+
+columnas <- colnames(RPM_cor)
+
+tabla_rpm <- list()
+for (i in c(1:11)) {
+  tabla_rpm[[i]] <- RPM_cor[, .(correlacion = cor(
+    get(columnas[i]),
+    get(columnas[12])
+  ), Pos = columnas[i]),
+  by = .(Chasis)
+  ]
+}
+RPM_cor <- rbindlist(tabla_rpm)
+rpm_cor_matrix <- dcast(RPM_cor, ... ~ Chasis, value.var = "correlacion")
+
+ggplot(data = RPM_cor) +
+  aes(x = Pos, y = correlacion, label = round(correlacion, 2)) +
+  geom_bar(stat = "identity", position = "dodge", fill = "lightblue") +
+  geom_text(nudge_y = 0.03) +
+  facet_grid(~Chasis) +
+  theme_minimal()
+
+# MAYORES vs MENORES -----------------------
+
+consumo_empleados <- merge.data.table(acelerador_41, consumo_41,
+  by = "cod_desig",
+  all.x = TRUE
+)
+consumo_empleados <- merge.data.table(consumo_empleados, chasis_41,
+  by = "cod_desig",
+  all.x = TRUE
+)
+consumo_empleados <- merge.data.table(consumo_empleados, empleados_41,
+  by = "cod_desig",
+  all.x = TRUE
+)
+consumo_empleados <- merge.data.table(consumo_empleados, tiempos_41,
+  by = "cod_desig",
+  all.x = TRUE
+)
+
+consumo_empleados <- consumo_empleados[`Suma de Distance` > 50, ]
+consumo_empleados <- consumo_empleados[`Suma de consumption.avgFuel` > 20, ]
+
+colnames(consumo_empleados)
+consumo_empleados <- consumo_empleados[, c(6, 8:10, 13, 14, 18, 19, 20, 21)]
+
+setnames(consumo_empleados, colnames(consumo_empleados), gsub("Suma de ", "", colnames(consumo_empleados)))
+
+consumo_empleados <- consumo_empleados[, .(
+  Distancia = sum(Distance, na.rm = TRUE),
+  Tiempo = sum(times.driving, na.rm = TRUE),
+  LTS = sum(consumption.fuel, na.rm = TRUE),
+  Acelerador_dist = weighted.mean(accelerator.get80, Distance, na.rm = TRUE),
+  Acelerador_time = weighted.mean(accelerator.get80, times.driving, na.rm = TRUE),
+  Acelerador_time_2 = weighted.mean(accelerator.get80, times.engine, na.rm = TRUE),
+  cant_servicios = .N
+), by = .(cod_emp)]
+
+consumo_empleados[, `:=`(
+  Consumo = LTS / Distancia * 100
+)]
+
+consumo_empleados <- consumo_empleados[cant_servicios > 20,]
+
+etiquetas_consumo <- c("Bajo", "Medio", "Alto")
+
+division_consumo <- quantile(consumo_empleados$Consumo, probs = c(0, 0.20, 0.80, 1))
+
+consumo_empleados[, Nivel_consumo := cut(Consumo,
+  breaks = division_consumo,
+  labels = etiquetas_consumo,
+  include.lowest = TRUE
+)]
+
+consumo_empleados_plot <- consumo_empleados[Nivel_consumo != "Medio", ]
+
+ggplot(consumo_empleados_plot) +
+  aes(x = Nivel_consumo, y = Acelerador_dist) +
+  geom_boxplot()
+
+
+ggplot(consumo_empleados_plot) +
+  aes(x = Nivel_consumo, y = Acelerador_time) +
+  geom_boxplot()
